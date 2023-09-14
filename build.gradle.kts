91854d2735fd600e3c5699a11cdc477bc47a7f09
Junichi Kato <j5ik2o@gmail.com>
2023-09-14 21:35:01 +0900

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,9 +46,9 @@ tasks {
         into("docs/javadoc")
     }
 
-//    assemble {
-//        dependsOn("javadocToDocsFolder")
-//    }
+    assemble {
+        dependsOn("javadocToDocsFolder")
+    }
 
     create<Jar>("sourcesJar") {
         from(sourceSets.main.get().allJava)
